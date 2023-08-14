33131503feec49755bd647413dabcfacaa3750c4
mauricio <maur28363429rd@gmail.com>
2023-08-14 09:14:46 -0400

    as

--- src/api/users/deleteUser.js
+++ src/api/users/deleteUser.js
@@ -1,6 +1,6 @@
 import { instance } from "..";
 
-export default async function deleteUser(id) {
+export default async function DeleteUser(id) {
   const { data } = await instance.delete("/users/delete/" + id);
   return data;
 }
